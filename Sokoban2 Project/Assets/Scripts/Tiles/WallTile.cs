355533193ec69d23e62255ad473d9666e78be55c
happy30 <jordivdh@hotmail.com>
2016-11-16 19:19:07 +0100

    Added some Tile Scripts

---
+++ Sokoban2 Project/Assets/Scripts/Tiles/WallTile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallTile : BaseTile
+{
+    public override void OnStep(Player player)
+    {
+        player.AbortStep();
+    }
+
+}
